65f8d05b9188a2d4b4b6683a0ebb20c97356b846
Anthony Hildoer <anthony@bluerival.com>
2020-07-08 07:54:59 -0600

    release 0.2.0

--- index.js
+++ index.js
@@ -8,247 +8,254 @@ const Sequelize = require( 'sequelize' );
 const Umzug = require( 'umzug' );
 
 let defaultConfig = {
-  connections: {}
+    connections: {}
 };
 
 let defaultConnectionConfig = {
-  database: 'mysql',
-  username: '',
-  password: '',
-  options: {
-    host: 'localhost',
-    port: 3306,
-    dialect: 'mysql',
-    // operatorsAliases: false, // default value in sequelize is true, which is also deprecated now... silly I know.
-    logging: false // another weird default that needs to be overridden
-  },
-  setupDir: null,
-  applyMigrations: false
+    database: 'mysql',
+    username: '',
+    password: '',
+    options: {
+        host: 'localhost',
+        port: 3306,
+        dialect: 'mysql',
+        // operatorsAliases: false, // default value in sequelize is true, which is also deprecated now... silly I know.
+        logging: false // another weird default that needs to be overridden
+    },
+    setupDir: null,
+    applyMigrations: false
 };
 
 class SequelizeComponent {
 
-  constructor( deps ) {
+    constructor( deps ) {
 
-    this._nconf = deps.get( 'config' );
+        this._nconf = deps.get( 'config' );
 
-    this._connectionModelsInitialized = {};
-    this._config = null;
-    this._connectionPool = {};
+        this._connectionModelsInitialized = {};
+        this._config = null;
+        this._connectionPool = {};
 
-  }
-
-  init( done ) {
-
-    if ( this._config ) {
-      return done( new Error( 'already initialized' ) );
     }
 
-    this._config = __( defaultConfig ).mixin( this._nconf.get( 'stringstack:sequelize' ) );
+    init( done ) {
 
-    this._initAllConnections( done );
+        if ( this._config ) {
+            return done( new Error( 'already initialized' ) );
+        }
 
-  }
+        this._config = __( defaultConfig ).mixin( this._nconf.get( 'stringstack:sequelize' ) );
 
-  dinit( done ) {
+        this._initAllConnections( done );
 
-    if ( !this._config ) {
-      return done( new Error( 'already d-initialized' ) );
     }
 
-    this._config = null;
+    dinit( done ) {
 
-    async.series( [
-      ( done ) => {
-        async.eachOfSeries( this._connectionPool, ( sequelize, name, done ) => {
+        if ( !this._config ) {
+            return done( new Error( 'already d-initialized' ) );
+        }
 
-          sequelize.close()
+        this._config = null;
+
+        async.series( [
+            ( done ) => {
+                async.eachOfSeries( this._connectionPool, ( sequelize, name, done ) => {
+
+                    sequelize.close()
+                        .then( () => {
+                            done();
+                        } )
+                        .catch( done );
+
+                }, done );
+            },
+            ( done ) => {
+                Object.keys( this._connectionPool ).forEach( ( name ) => {
+                    delete this._connectionPool[name];
+                } );
+                done();
+            }
+        ], done );
+
+    }
+
+    getConnection( connectionName, done = null ) {
+
+        if ( typeof done !== 'function' ) {
+
+            done = ( e, connection ) => {
+
+                return new Promise( ( resolve, reject ) => {
+                    if ( e ) {
+                        return reject( e );
+                    }
+
+                    resolve( connection );
+                } );
+
+            }
+        }
+
+        if ( !this._config ) {
+            return done( new Error( 'not initialized' ) );
+        }
+
+        if ( this._connectionPool.hasOwnProperty( connectionName ) ) {
+            return done( null, this._connectionPool[connectionName] );
+        }
+
+        if ( !this._config.connections.hasOwnProperty( connectionName ) ) {
+            return done( new Error( 'connection identifier not found' ) );
+        }
+
+        let config = __( defaultConnectionConfig ).mixin( this._config.connections[connectionName] );
+
+        let { database, username, password, options } = config;
+
+        let sequelize = new Sequelize( database || null, username || null, password || null, options || {} );
+
+        return sequelize
+            .authenticate()
             .then( () => {
-              done();
+
+                this._connectionPool[connectionName] = sequelize;
+
+                return done( null, sequelize );
+
             } )
             .catch( done );
 
+    }
+
+    _initAllConnections( done ) {
+
+        async.eachOfSeries( this._config.connections, ( connectionConfig, connectionName, done ) => {
+            this._initConnection( connectionName, connectionConfig, done );
         }, done );
-      },
-      ( done ) => {
-        Object.keys( this._connectionPool ).forEach( ( name ) => {
-          delete this._connectionPool[name];
-        } );
-        done();
-      }
-    ], done );
 
-  }
-
-  getConnection( connectionName, done ) {
-
-    if ( !this._config ) {
-      return done( new Error( 'not initialized' ) );
     }
 
-    if ( this._connectionPool.hasOwnProperty( connectionName ) ) {
-      return done( null, this._connectionPool[connectionName] );
-    }
+    _initConnection( connectionName, connectionConfig, done ) {
 
-    if ( !this._config.connections.hasOwnProperty( connectionName ) ) {
-      return done( new Error( 'connection identifier not found' ) );
-    }
+        if ( this._connectionModelsInitialized[connectionName] ) {
+            return done();
+        }
+        this._connectionModelsInitialized[connectionName] = true;
 
-    let config = __( defaultConnectionConfig ).mixin( this._config.connections[connectionName] );
+        connectionConfig = this._normalizeConnectionConfig( connectionConfig );
 
-    let { database, username, password, options } = config;
-
-    let sequelize = new Sequelize( database || null, username || null, password || null, options || {} );
-
-    async.waterfall( [
-      ( done ) => {
-
-        sequelize
-          .authenticate()
-          .then( () => {
-            done();
-          } )
-          .catch( done );
-
-      },
-      ( done ) => {
-
-        this._connectionPool[connectionName] = sequelize;
-        done( null, sequelize );
-
-      }
-    ], done );
-
-  }
-
-  _initAllConnections( done ) {
-
-    async.eachOfSeries( this._config.connections, ( connectionConfig, connectionName, done ) => {
-      this._initConnection( connectionName, connectionConfig, done );
-    }, done );
-
-  }
-
-  _initConnection( connectionName, connectionConfig, done ) {
-
-    if ( this._connectionModelsInitialized[connectionName] ) {
-      return done();
-    }
-    this._connectionModelsInitialized[connectionName] = true;
-
-    connectionConfig = this._normalizeConnectionConfig( connectionConfig );
-
-    if ( typeof connectionConfig.setupDir !== 'string' || connectionConfig.setupDir.trim().length < 1 ) {
-      return done();
-    }
-
-    let setupDir = connectionConfig.setupDir.trim();
-    if ( setupDir.match( /^\./ ) ) {
-      setupDir = Path.join( process.cwd(), setupDir );
-    }
-    setupDir = Path.normalize( setupDir );
-
-    async.waterfall( [
-      ( done ) => {
-        this.getConnection( connectionName, done );
-      },
-      ( sequelize, done ) => {
-
-        if ( connectionConfig.applyMigrations ) {
-          this._initMigrations( setupDir, sequelize, done );
-        } else {
-          setImmediate( done, null, sequelize );
+        if ( typeof connectionConfig.setupDir !== 'string' || connectionConfig.setupDir.trim().length < 1 ) {
+            return done();
         }
 
-      },
-      ( sequelize, done ) => {
-        this._initModels( setupDir, sequelize, done );
-      }
-    ], done );
+        let setupDir = connectionConfig.setupDir.trim();
+        if ( setupDir.match( /^\./ ) ) {
+            setupDir = Path.join( process.cwd(), setupDir );
+        }
+        setupDir = Path.normalize( setupDir );
 
-  }
+        async.waterfall( [
+            ( done ) => {
+                this.getConnection( connectionName, done );
+            },
+            ( sequelize, done ) => {
 
-  _initMigrations( setupDir, sequelize, done ) {
+                if ( connectionConfig.applyMigrations ) {
+                    this._initMigrations( setupDir, sequelize, done );
+                } else {
+                    setImmediate( done, null, sequelize );
+                }
 
-    let umzug = new Umzug( {
-      storage: 'sequelize',
-      storageOptions: {
-        sequelize: sequelize,
-        tableName: 'SequelizeMeta'
-      },
-      migrations: {
-        path: Path.join( setupDir, 'migrations' ),
-        params: [ sequelize, Sequelize ]
-      },
-      logging: function () {
-        // TODO hook this to logging harness
-      }
-    } );
-
-    umzug.up()
-      .then( () => {
-        setImmediate( done, null, sequelize );
-      } )
-      .catch( done );
-
-  }
-
-  _initModels( setupDir, sequelize, done ) {
-
-    let modelsDir = Path.join( setupDir, 'models' );
-
-    async.waterfall( [
-      ( done ) => {
-        fs.readdir( modelsDir, done );
-      },
-      ( files, done ) => {
-
-        files = files
-          .filter( ( file ) => {
-            return typeof file === 'string' && file.match( /^[a-zA-Z][a-zA-Z0-9]*\.model\.js$/ );
-          } )
-          .map( ( file ) => {
-
-            let parts = file.split( '.' );
-
-            return {
-              path: Path.join( modelsDir, file ),
-              modelName: parts[0]
-            };
-
-          } );
-
-        try {
-
-          files.forEach( ( file ) => {
-
-            let loader = require( file.path );
-            let model = loader( Sequelize );
-
-            if ( !model.schema ) {
-              throw new Error( 'schema field required' );
+            },
+            ( sequelize, done ) => {
+                this._initModels( setupDir, sequelize, done );
             }
+        ], done );
 
-            sequelize.define( file.modelName, model.schema, model.options || {} );
+    }
 
-          } );
+    _initMigrations( setupDir, sequelize, done ) {
 
-        } catch ( e ) {
-          return done( e );
-        }
+        let umzug = new Umzug( {
+            storage: 'sequelize',
+            storageOptions: {
+                sequelize: sequelize,
+                tableName: 'SequelizeMeta'
+            },
+            migrations: {
+                path: Path.join( setupDir, 'migrations' ),
+                params: [ sequelize, Sequelize ]
+            },
+            logging: function () {
+                // TODO hook this to logging harness
+            }
+        } );
 
-        done();
+        umzug.up()
+            .then( () => {
+                setImmediate( done, null, sequelize );
+            } )
+            .catch( done );
 
-      }
-    ], done );
+    }
 
-  }
+    _initModels( setupDir, sequelize, done ) {
 
-  _normalizeConnectionConfig( connectionConfig ) {
+        let modelsDir = Path.join( setupDir, 'models' );
 
-    return __( defaultConnectionConfig ).mixin( connectionConfig );
+        async.waterfall( [
+            ( done ) => {
+                fs.readdir( modelsDir, done );
+            },
+            ( files, done ) => {
 
-  }
+                files = files
+                    .filter( ( file ) => {
+                        return typeof file === 'string' && file.match( /^[a-zA-Z][a-zA-Z0-9]*\.model\.js$/ );
+                    } )
+                    .map( ( file ) => {
+
+                        let parts = file.split( '.' );
+
+                        return {
+                            path: Path.join( modelsDir, file ),
+                            modelName: parts[0]
+                        };
+
+                    } );
+
+                try {
+
+                    files.forEach( ( file ) => {
+
+                        let loader = require( file.path );
+                        let model = loader( Sequelize );
+
+                        if ( !model.schema ) {
+                            throw new Error( 'schema field required' );
+                        }
+
+                        sequelize.define( file.modelName, model.schema, model.options || {} );
+
+                    } );
+
+                } catch ( e ) {
+                    return done( e );
+                }
+
+                done();
+
+            }
+        ], done );
+
+    }
+
+    _normalizeConnectionConfig( connectionConfig ) {
+
+        return __( defaultConnectionConfig ).mixin( connectionConfig );
+
+    }
 
 
 }
